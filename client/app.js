7248b6ad91855ffd54db611e9805b820df9e3a14
DavidUllauri <ullauridavid@gmail.com>
2022-06-25 19:31:01 -0400

    Added room feature

--- client/app.js
+++ client/app.js
@@ -2,58 +2,73 @@ const socket = io('ws://localhost:8080');
 
 const nColors = 20;
 let colorMap = {}
+let roomID = '';
 
 function randInt(max) { return Math.floor( Math.random() * max ); }
 
 let hue = randInt(nColors) * (360 / nColors);
-
 
 function addDynamicStyles() {
     document.querySelector('button').style.color = `hsl(${hue}, 70%, 55%)`;
     
     const css = `button:hover{ background-color: hsl(${hue}, 70%, 55%); color: #000 !important; cursor: pointer; }`
     let style = document.createElement('style');
+
     if (style.styleSheet) {
         style.styleSheet.cssText = css;
     } else {
         style.appendChild(document.createTextNode(css));
     }
+
     document.getElementsByTagName('head')[0].appendChild(style);
 }
 addDynamicStyles();
 
-// Listen for messages
-socket.on('message', (name, message) => {
+function appendMessage(name, message) {
     if (colorMap[name] == null) {
         hue = randInt(nColors) * (360 / nColors);
         colorMap[name] = hue;
     }
+
     const el = document.createElement('div');
-    el.className = "msg-item";
-    
     const profile = document.createElement('div');
-    console.log(colorMap[name]);
+    const msgbody = document.createElement('div');
+    const pmsg = document.createElement('p');
+    const chat = document.getElementById('chat');
+    
+    el.className = "msg-item";
+    el.appendChild(profile);
+    el.appendChild(msgbody);
+
     profile.style.backgroundColor = `hsl(${colorMap[name]}, 70%, 55%)`;
     profile.className = "profile-img";
     profile.innerText = name;
-    el.appendChild(profile);
 
-    const msgbody = document.createElement('div');
     msgbody.className = "msg-body";
-    el.appendChild(msgbody);
-
-    const pmsg = document.createElement('p');
-    pmsg.innerText = message;
     msgbody.appendChild(pmsg);
 
-    document.getElementById('chat').firstElementChild.appendChild(el);
+    pmsg.innerText = message;
+
+    chat.firstElementChild.appendChild(el);
+    chat.scrollTop= chat.scrollHeight;
+}
+
+// Listen for messages
+socket.on('receive-message', (name, message) => {
+    appendMessage(name, message);
 });
 
-document.getElementById('message-form').addEventListener('submit', (event) => {
-    event.preventDefault();
+document.getElementById('message-form').addEventListener('submit', (e) => {
+    e.preventDefault();
     const msg = document.getElementById('msg-i');
-
-    socket.emit('message', msg.value);
-    
+    socket.emit('send-message', msg.value, roomID);
     msg.value = "";
+});
+
+document.getElementById('room-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const room = document.getElementById('room-i');
+    socket.emit('join-room', room.value);
+    roomID = room.value;
+    room.value = "";
 });
--- server/index.js
+++ server/index.js
@@ -5,12 +5,22 @@ const io = require('socket.io')(http, {
 port = 8080;
 
 io.on("connection", (socket) => {
-    console.log('A user connected');
-    socket.on('message', (message) => {
+    console.log(`${socket.id} connected`);
+    let name = socket.id.substr(0,2);
+
+    socket.on('send-message', (message, room) => {
         console.log(message);
-        let name = socket.id.substr(0,2);
-        io.emit('message', name, message);
+        if (room === '') {
+            io.emit('receive-message', name, message);
+        } else {
+            socket.nsp.to(room).emit('receive-message', name, message);
+        }
+    });
+    socket.on('join-room', (room) => {
+        console.log(`${socket.id} joined ${room}`);
+        socket.join(room);
+        socket.nsp.to(room).emit('receive-message', name, `joined ${room} room`);
     });
 });
 
-http.listen(port, () => console.log(`Listening on http://localhost:${port}`))
+http.listen(port, () => console.log(`Listening on http://localhost:${port}`));
